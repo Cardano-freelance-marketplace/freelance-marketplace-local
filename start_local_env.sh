4298ad6fd763989a06530cc74ea44ddf5adebb0d
Joao Velez <joao.velez2020@hotmail.com>
2025-06-20 00:55:09 +0100

    Attempt at fixing cardano-submit-api not finding /ipc/node.socket

--- start_local_env.sh
+++ start_local_env.sh
@@ -2,3 +2,13 @@
 
 NETWORK=preview docker compose up -d --build
 
+# Wait for the container to be up and the socket to exist
+while ! docker exec freelance_cardano_node test -S /ipc/node.socket; do
+  echo "Waiting for /ipc/node.socket to be created..."
+  sleep 1
+done
+
+echo "Running chmod 777 on /ipc/node.socket"
+# Change permissions inside the container
+docker exec freelance_cardano_node chmod 777 /ipc/node.socket
+
